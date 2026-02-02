f1e9631b1c2e415224b7bd67d135fb2af9e21779
jihyun <gkang0702@gmail.com>
2026-02-02 22:07:36 +0900

    Add image source update functionality
    Set an interval to change all image sources every 500ms.

---
+++ Image_2_speaki/index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) => {
+    a.src = 'https://i.namu.wiki/i/Ob0--Jok1pkNtNu46VjPmTZWbmaql5Xf0pexZ5RBz3B5Nlj8z_xqYSyMqw70Ad5mEYa_i1GHcHda5pbilvBNOA.webp';
+})
+
+}, 500);
