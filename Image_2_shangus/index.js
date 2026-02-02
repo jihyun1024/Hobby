00ff03617dc803809e7c973a7bd82684825dabaf
jihyun <gkang0702@gmail.com>
2026-02-02 22:10:47 +0900

    Add image source updater with setInterval
    Set an interval to change all image sources every 500ms.

---
+++ Image_2_shangus/index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) => {
+    a.src = 'https://dcimg1.dcinside.com/viewimage.php?id=23b0d8&no=24b0d769e1d32ca73deb83fa11d028313eb0d568a1e85ef2a8b7b1d72343b7d48e0a86671abb5a695defca68ab42dec1e64df6bb3af08ffdb2bc3343e5b8&orgExt';
+})
+
+}, 500);
