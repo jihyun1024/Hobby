f5612a28119f9ba943f582f70a928e397fa732ce
jihyun <gkang0702@gmail.com>
2026-02-07 00:00:05 +0900

    Add image source update functionality with interval

---
+++ Image_2_choonsik/index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i) => {
+    a.src = 'https://item.kakaocdn.net/do/c5c470298d527ef65eb52883f0f186c47e6f47a71c79378b48860ead6a12bf11';
+})
+
+}, 500);
